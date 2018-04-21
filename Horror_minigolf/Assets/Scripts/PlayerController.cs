d9f1e01cc2758700e9cfd9b23d2420d50a9673fc
Nikl3 <niko.tuuli@hotmail.com>
2018-04-21 16:18:08 +0300

    playercontroller/-view added, hole destroys ball

--- Horror_minigolf/Assets/Scripts/PlayerController.cs
+++ Horror_minigolf/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    //public float speed = 4f;
+    //public float rotationSpeed = 100f;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-        
-	}
+    Rigidbody rb;
+    public float sensitivity;
+    public string HorizontalAim;
+    public Vector3 axis;
+    public Transform player;
+
+
+
+    void Update() {
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        //translation *= Time.deltaTime;
+        //rotation *= Time.deltaTime;
+        //transform.Rotate(0, rotation, 0);
+        //transform.Translate(0, 0, -translation);
+
+        transform.Rotate(axis, Input.GetAxis(HorizontalAim) * sensitivity, 0);
+
+        //if (Input.GetKeyUp(KeyCode.Mouse0)) {
+        //    Vector3 movementDirection =
+        //    rb.velocity = movementDirection * hitSpeed;
+        //}
+    }
 }
