78ec63c03119cf22a010dbe62ae9c4ed38a48e3c
OPISTO\aaro.leskinen <A>
2018-04-21 16:17:34 +0300

    Ball moving direction script.

--- Horror_minigolf/Assets/Scripts/BallAimDirection.cs
+++ Horror_minigolf/Assets/Scripts/BallAimDirection.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class BallAimDirection : MonoBehaviour {
 
-    public Transform rayStartingPoint;
+   // public Transform rayStartingPoint;
+    public Transform beam;
+    public float beamLenght;
 
     void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            beam.gameObject.SetActive(true);
+            beam.localScale = new Vector3(1, 1, beamLenght);
+        }
+         if(Input.GetMouseButtonUp(0)) {
+            beam.gameObject.SetActive(false);
+        }
+
+        /*
+        RaycastHit hitInfo;
+        
+        Physics.Raycast(rayStartingPoint.position, rayStartingPoint.forward, out hitInfo);
+        
+        
+
+    */
+
+
+
+
+
+
+        
 
 
     }
