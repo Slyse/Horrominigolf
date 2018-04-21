614772832c6bd3ea66e27482ea5933a258e97408
OPISTO\aaro.leskinen <A>
2018-04-21 13:35:52 +0300

    Resetoin objektien transformin. Tein kansioita ja aloittelin ray scriptiä.

---
+++ Horror_minigolf/Assets/Scripts/BallAimDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAimDirection : MonoBehaviour {
+
+    public Transform rayStartingPoint;
+
+    void Update() {
+
+
+    }
+
+}
