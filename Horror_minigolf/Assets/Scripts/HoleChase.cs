67fbae7a27f57a7a1e74744e36533fd05fac34da
OPISTO\aaro.leskinen <A>
2018-04-22 11:11:02 +0300

    Holechase skipti. Hole jahtaa pelaajaa.

---
+++ Horror_minigolf/Assets/Scripts/HoleChase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleChase : MonoBehaviour {
+    public Transform nikoEyeBall;
+    public float speed;
+    int checkpoints;
+     public int maxCheckpoints;
+
+    void Update() {
+
+        float move = speed * Time.deltaTime;
+
+        while ( checkpoints < maxCheckpoints) {
+            if(checkpoints == 1) {
+                transform.position = Vector3.MoveTowards(transform.position, nikoEyeBall.position, move);
+            }
+        }
+
+
+
+        
+        
+    }
+
+}
