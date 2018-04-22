fb0df18041f2cca1ced3de1f5fd07cfb3d429478
OPISTO\oskari.hermunen <ozzah7@hotmail.com>
2018-04-22 11:15:09 +0300

    Holescript changed.

--- Horror_minigolf/Assets/Scripts/HoleChase.cs
+++ Horror_minigolf/Assets/Scripts/HoleChase.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class HoleChase : MonoBehaviour {
     public Transform nikoEyeBall;
     public float speed;
-    int checkpoints;
-     public int maxCheckpoints;
+   // int checkpoints;
+    // public int maxCheckpoints;
 
     void Update() {
 
         float move = speed * Time.deltaTime;
 
-        while ( checkpoints < maxCheckpoints) {
-            if(checkpoints == 1) {
+        //while ( checkpoints < maxCheckpoints) {
+            //if(checkpoints == 1) {
                 transform.position = Vector3.MoveTowards(transform.position, nikoEyeBall.position, move);
-            }
-        }
+           // }
+        //}
 
 
 
